8e8c69cb418ae7db1f63d4620c64e3bed30c840e
Olivier <Olivier@LOK111-03>
2018-01-05 09:19:31 +0100

    + Begroetingclass

--- WebContent/WEB-INF/JSP/index.jsp
+++ WebContent/WEB-INF/JSP/index.jsp
@@ -1,5 +1,4 @@
 <%-- Een welkom pagina --%>
-<%@page import="java.time.LocalDateTime"%>
 <%@page contentType="text/html" pageEncoding="UTF-8" session="false"%>
 <!DOCTYPE html>
 <html lang="nl">
@@ -7,7 +6,7 @@
 		<title>Pizza Luigi</title>
 		<link rel="icon" href="images/favicon.ico">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" href="styses/default.css">
+		<link rel="stylesheet" href="styles/default.css">
 	</head>
 	<body>
 		<h1>Pizza Luigi</h1>
--- src/be/vdab/servlets/IndexServlet.java
+++ src/be/vdab/servlets/IndexServlet.java
@@ -1,7 +1,6 @@
 package be.vdab.servlets;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.time.LocalDateTime;
 
 import javax.servlet.ServletException;
@@ -9,6 +8,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import be.vdab.entities.Begroeting;
 
 @WebServlet("/index.htm")
 public class IndexServlet extends HttpServlet {
@@ -19,8 +20,7 @@ public class IndexServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		int uur = LocalDateTime.now().getHour();
-		request.setAttribute("begroeting", uur >= 6 && uur < 12 ? "Goede morgen" :
-			uur >= 12 && uur <18 ? "Goede middag" : "Goede Avond");
+		request.setAttribute("begroeting", new Begroeting());
 		request.getRequestDispatcher(VIEW).forward(request, response);
 	}
 
